5943136a7c95775786fffb9508d8960febb2a971
Shubham <shivam828787@gmail.com>
2022-12-26 02:05:14 +0530

    dynamic result

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,7 +5,7 @@ import { faCompressAlt } from '@fortawesome/free-solid-svg-icons'
 import Guage from './result-components/Guage'
 import ExploreDashboard from './result-components/ExploreDashboard'
 
-function Results() {
+function Results({data}) {
     return (
         <div className='board'>
             <div className="editor-title">
@@ -16,26 +16,32 @@ function Results() {
             </div>
             <div className='board-area relative'>
                 <div className='flex flex-wrap'>
+                    {data.latencyP95 &&
                     <div className='latency-guage m-2 w-48 h-30'>
-                        <Guage label="Latency (P95)" value={0.3} formatText={(value) => value + 'ms'} />
-                    </div>
+                        <Guage label="Latency (P95)" value={data.latencyP95} formatText={(value) => value + 'ms'} />
+                    </div> || null}
+                    {data.throughput &&
                     <div className='latency-guage m-2 w-48 h-30'>
-                        <Guage label="Req handled per sec" value={0.14} formatText={(value) => value + 'k'} />
-                    </div>
+                        <Guage label="Req handled per sec" value={data.throughput} formatText={(value) => value + 'k'} />
+                    </div> || null}
+                    {data.cpuBusy &&
                     <div className='latency-guage m-2 w-48 h-30'>
-                        <Guage label="CPU Busy" value={0.6} />
-                    </div>
+                        <Guage label="CPU Busy" value={data.cpuBusy} />
+                    </div> || null}
+                    {data.ramUsed &&
                     <div className='latency-guage m-2 w-48 h-30'>
                         <Guage label="RAM Used" value={0.14} />
-                    </div>
+                    </div> || null}
+                    {data.load5m &&
                     <div className='latency-guage m-2 w-48 h-30'>
                         <Guage label="Load (5m)" value={0.14} />
-                    </div>
+                    </div> || null}
+                    {data.swapUsed &&
                     <div className='latency-guage m-2 w-48 h-30'>
                         <Guage label="SWAP Used" value={0} />
-                    </div>
+                    </div> || null}
                 </div>
-                <ExploreDashboard link='yo' />
+                {data.dashboardLink && <ExploreDashboard link={data.dashboardLink} /> || null}
             </div>
         </div>
     )
