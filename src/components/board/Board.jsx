73c9338753555b95c5d77d4ff2fd816336566e90
Shubham <shivam828787@gmail.com>
2023-01-12 03:13:04 +0530

    added dotted box

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -32,26 +32,25 @@ function Board({ resourcePool, onRunTest }) {
                 />
             </div>
             <div className='board-area relative drop-box shadow-lg p-4 bg-gray-800'>
-                {/* <DndProvider backend={HTML5Backend}>
-            <div className="App">
-                <DragDrop resourcePool={resourcePool}/>
-            </div>
-        </DndProvider> */}
-                {resourcePool.map((item) => {
-                    // return <Picture url={PictureList[item.tag[0]]} id={item.id} name={item.name} description={item.description} isDropped={isDropped(item.name)}/>;
-                    return (<Draggable
-                        defaultPosition={{ x: 100, y: 150 }}
-                        position={null}
-                        grid={[50, 50]}
-                        scale={1}
-                    > 
-                            <Resource 
+                <div className='border-2 border-dotted w-1/2 h-1/2 relative top-[15%] left-1/4'>
+                    {resourcePool.map((item) => {
+                        // return <Picture url={PictureList[item.tag[0]]} id={item.id} name={item.name} description={item.description} isDropped={isDropped(item.name)}/>;
+                        return (<Draggable
+                            defaultPosition={{ x: 120, y: 120 }}
+                            position={null}
+                            grid={[50, 50]}
+                            scale={1}
+                            bounds="parent"
+                        >
+                            <Resource
                                 name={item.name}
                                 url={PictureList['vm']}
                                 id={item.index}
                             />
-                    </Draggable>
-                )})}
+                        </Draggable>
+                        )
+                    })}
+                </div>
                 <Panel resourcePool={resourcePool} onRunTest={onRunTest} />
             </div>
         </div>
