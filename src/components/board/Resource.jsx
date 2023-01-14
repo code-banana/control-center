5687f013750efaf52561b285807565289db85119
Shubham <shivam828787@gmail.com>
2023-01-15 00:45:25 +0530

    made drggable

--- src/components/board/Resource.jsx
+++ src/components/board/Resource.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useDrag } from "react-dnd";
 
-function Resource({ id, url, name, ...rest }) {
+function Resource({ id, url, name, className, ...rest }) {
+
   return (
-    <div {...rest}>
-    <p className={"relative h-fit w-fit "}>
+    <div className={" h-fit w-fit "+className} {...rest}>
+    <p className={"relative"}>
     <img
       className="h-20 w-20 m-2"
       src={url}
@@ -13,7 +14,7 @@ function Resource({ id, url, name, ...rest }) {
     />
       <span className="absolute top-0 mt-8 mx-4 text-black whitespace-prewrap">{name}</span>
     </p>
-    </div> 
+    </div>
   );
 }
 
